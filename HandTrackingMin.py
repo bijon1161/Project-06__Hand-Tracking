74000e6e0325a6064539469a2ac8e5dc6eb1d8cf
bijon <bijonndc@gmail.com>
2021-05-29 14:30:36 +0600

    Tracking My Hand or Not

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -10,8 +10,14 @@ import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
 while True:
     success, img = cap.read()
-    
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    print(results.multi_hand_landmarks)
     cv2.imshow("Image",img)
     cv2.waitKey(1)
