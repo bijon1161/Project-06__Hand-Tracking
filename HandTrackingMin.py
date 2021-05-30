bf1b63452176081bc7d883ffc6851a385229ac47
bijon <bijonndc@gmail.com>
2021-05-30 12:02:56 +0600

    Fps Detection

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -14,6 +14,10 @@ cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw =mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -22,6 +26,11 @@ while True:
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
-            
+     
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_COMPLEX_SMALL, 3, (255,0,255),3)
     cv2.imshow("Image",img)
     cv2.waitKey(1)
