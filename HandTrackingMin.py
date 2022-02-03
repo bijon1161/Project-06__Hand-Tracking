91ad978abefb3895a758ab6550cbc03d2bc97f23
Bijon Mallik <bijon.ndc.pust@gmail.com>
2022-02-03 09:07:12 +0600

    Minimum code is done,time to make module

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -25,6 +25,13 @@ while True:
     #print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                print(id,lm)
+                h,w,c=img.shape
+                cx,cy = int(lm.x*w),int(lm.y*h)
+                
+                cv2.circle(img,(cx,cy),15,(255,8,255),cv2.FILLED)
+            
             mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
      
     cTime = time.time()
